0e27f83c7f1e6c9a8400bb2c49b8890b83df130e
Manikanta1249 <methukumanikanta60@gmail.com>
2026-08-24 10:44:26 +0530

    Create 9 exp.c

---
+++ Lab Experiments/9 exp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+    char *str = (char *)shmat(shmid, NULL, 0);
+
+    if (fork() == 0) {
+        printf("Child reads: %s\n", str);
+        shmdt(str);
+    } else {
+        strcpy(str, "Hello from Parent Process");
+        wait(NULL);
+        shmdt(str);
+        shmctl(shmid, IPC_RMID, NULL);
+    }
+
+    return 0;
+}

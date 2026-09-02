386304af8e91af745cad35aa1a7195c000c0ee92
Manikanta1249 <methukumanikanta60@gmail.com>
2026-09-02 11:55:22 +0530

    Add page replacement algorithm in 31.c
    Implement a page replacement algorithm in C.

---
+++ Lab Experiments/31.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int pages[20], frame[10], n, f, i, j, k = 0, fault = 0, found;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+    printf("Enter page reference string: ");
+    for(i = 0; i < n; i++) scanf("%d", &pages[i]);
+
+    printf("Enter number of frames: ");
+    scanf("%d", &f);
+
+    for(i = 0; i < f; i++) frame[i] = -1;
+
+    for(i = 0; i < n; i++) {
+        found = 0;
+        for(j = 0; j < f; j++)
+            if(frame[j] == pages[i]) found = 1;
+
+        if(!found) {
+            frame[k] = pages[i];
+            k = (k + 1) % f;
+            fault++;
+        }
+
+        printf("\n");
+        for(j = 0; j < f; j++)
+            printf("%d ", frame[j]);
+    }
+
+    printf("\nPage Faults = %d\n", fault);
+    return 0;
+}
